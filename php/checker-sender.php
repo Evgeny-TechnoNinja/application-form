2b8f880dfda89c194acda3cfdc4fbcdeffc6b07d
Eugene-TechnoNinja <technoninja@ukr.net>
2018-07-07 19:11:02 +0300

    Changed the approach to the location of the php script and its execution, added new files. The main emphasis is on the php script.

--- php/checker-sender.php
+++ php/checker-sender.php
@@ -1,12 +1,13 @@
-<?php 
-//Реализую форму обратной связи.
+<?php
+//Скрипт CHECKER-SENDER
+//Реализую работу формы обратной связи.
 //Запускаю сессию
 session_start();
 //Переменные
 $error = false;
 //Удаляю предыдущие значения из сессии.
-unset($_SESSION['name']);
-unset($_SESSION['phone']);
+//unset($_SESSION['name']);
+//unset($_SESSION['phone']);
 //Определяю send не NULL (то-есть нажата была кнопка "Записаться") тогда...
 if(isset($_POST['send'])) {
     //Записываю данные из массива в переменные. 
@@ -47,6 +48,11 @@ if(isset($_POST['send'])) {
  //Если ошибок нет тогда уведомляю что пользователь записан.
 if (!$error) {
     $notice = "<span style = color:#19c119;>Вы записаны!</span>";
+//Удаляю значения из сессии.
+    unset($_SESSION['name']);
+    unset($_SESSION['phone']);
+//перенаправление на страницу
+    header('Refresh: 7; ../index.html');
 }
 //Если имя  пусто, то уведомления тоже пустое.    
 if($name == ""){
@@ -54,48 +60,7 @@ if($name == ""){
 }
 //Если имя  пусто, а телефон нет, то...
 if($name == "" && $phone != ""){
-     $notice = "Укажите имя и фамилию!";
+     $notice = "Укажите имя и фамилию!"; 
 }
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PHP HomeWork Four</title>
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link href="https://fonts.googleapis.com/css?family=Oswald&amp;subset=cyrillic" rel="stylesheet">
-  <link rel="stylesheet" href="css/normalize.min.css">
-  <link rel="stylesheet" href="css/final-style.css">
-</head>
-<body class="page">
-
-    <div class="container">
-
-        <header class="header container__header">
-            <p class="header__txt">Вы видите, как я выполнил своё четвертое&ensp;<a href="doc/php-hw-4.pdf" target="_blank" class="header__link header__link_hover">домашнее задание</a>&ensp;по программе "Марафон по PHP".</p>
-        </header>
-        
-        <div class="application container__application">
-            <h1 class="application__heading">Добровольцы для колонизации Марса</h1>
-           
-            <form class="form application__form" action="index.php" method="POST" name="applicationForm">
-                <p class="form__txt">Запись на собеседование</p>
-                <input class="form__field form__field_active" placeholder="Ваше имя и фамилия" name="name" maxlength="20" type="text" value="<? echo $_SESSION['name'] ?>"><br>
-                <input class="form__field form__field_active" placeholder="Ваш номер телефона" name="phone" maxlength="20" type="tel" value="<? echo $_SESSION['phone'] ?>"><br>
-                <button class="form__btn form__btn_hover" name="send" type="submit">Записаться</button>
-                <p class="form__txt-notice"><? echo $notice; ?></p> 
-            </form>
-        
-        </div>
-        
-        <footer class="footer container__footer">
-        <p class="footer__txt">Благодарю замечательного художника <a href="https://www.artstation.com/steveburg" target="_blank"  class="footer__link footer__link_hover">Steve Burg</a> за прекрасное изображение.</p>
-        <p class="footer__text-author"><a href="https://www.facebook.com/baranovevgene" target="_blank" class="footer__link footer__link_text_bright">&copy;2018*TechnoNinja*</a></p>
-        </footer>
-    </div>
-
-</body>
